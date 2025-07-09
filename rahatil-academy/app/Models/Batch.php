07fd270cce802f8fd0817974f606670789d6c091
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-09 13:57:24 +0600

    all models are created

--- rahatil-academy/app/Models/Batch.php
+++ rahatil-academy/app/Models/Batch.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Batch extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'course_id', 'start_date'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
 }
