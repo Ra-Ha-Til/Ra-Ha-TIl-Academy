1dd910a2f9ae00fa007d09dc5e0a2584b48c9db3
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-09 14:00:32 +0600

    route setted

--- rahatil-academy/routes/web.php
+++ rahatil-academy/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\BatchController;
+use App\Http\Controllers\EnrollmentController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('students', StudentController::class);
+Route::resource('teachers', TeacherController::class);
+Route::resource('courses', CourseController::class);
+Route::resource('batches', BatchController::class);
+Route::resource('enrollments', EnrollmentController::class);
+Route::resource('payments', PaymentController::class);
