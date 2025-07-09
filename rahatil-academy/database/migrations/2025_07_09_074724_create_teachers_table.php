e4f933f733bc65f5234de7ec18ec8856fe415858
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-09 13:50:58 +0600

    teacher database added

--- rahatil-academy/database/migrations/2025_07_09_074724_create_teachers_table.php
+++ rahatil-academy/database/migrations/2025_07_09_074724_create_teachers_table.php
@@ -6,21 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('teachers');
     }
