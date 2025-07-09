135e9da6660e0f5b217b05322003660fd99f9d47
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-09 13:51:27 +0600

    courses table  added

--- rahatil-academy/database/migrations/2025_07_09_074745_create_courses_table.php
+++ rahatil-academy/database/migrations/2025_07_09_074745_create_courses_table.php
@@ -6,21 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('syllabus');
+            $table->string('duration');
+            $table->foreignId('teacher_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('courses');
     }
