5bf63e4208494a88712d5e28a287dcca4a0ec294
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-09 13:56:35 +0600

    course model created

--- rahatil-academy/app/Models/Course.php
+++ rahatil-academy/app/Models/Course.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'syllabus', 'duration', 'teacher_id'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function batches()
+    {
+        return $this->hasMany(Batch::class);
+    }
 }
